9d02292d009b21833e6410437ce86e082b82532a
HarikiRito <nxh0809@gmail.com>
2019-03-04 16:59:03 +0700

    .

---
+++ ash/__init__.py
@@ -0,0 +1,41 @@
+
+unsorted = [20, 100, 5, 50, 20, 20, 100, 5, 20, 7]
+
+
+def swap(a, b):
+    temp = a
+    a = b
+    b = temp
+    return [a, b]
+
+
+def mergeSort(alist):
+    print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = j = k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+    print("Merging ", alist)
---
+++ sorting.py
@@ -0,0 +1,51 @@
+from ash import swap, unsorted, mergeSort
+from typing import List
+import numpy as np
+
+
+def bubble_sort(alist: List[str]):
+    total_range = range(len(alist) - 1);
+    for j in total_range:
+        for i in total_range:
+            if alist[i] > alist[i + 1]:
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+    print(alist)
+
+
+def insertion_sort(alist):
+    total_range = range(1, len(alist))
+    for i in total_range:
+        value_insert = alist[i]
+        hold = i
+
+        while hold > 0 and alist[hold - 1] > value_insert:
+            alist[hold] = alist[hold - 1]
+            hold -= 1
+
+        alist[hold] = value_insert
+
+    print(alist)
+
+
+def selectionSort(alist):
+    for i in range(len(alist)):
+        index_min = i
+
+        for j in range(i + 1, len(alist)):
+            if alist[j] < alist[index_min]:
+                index_min = j
+
+        if index_min != i:
+            temp = alist[index_min]
+            alist[index_min] = alist[i]
+            alist[i] = temp
+
+    print(alist)
+
+
+# bubble_sort(unsorted)
+# insertion_sort(unsorted)
+# selectionSort(unsorted)
+mergeSort(unsorted)
